9eaa8a7435f6a45265aaafb487a5e9a2551b30f0
AmbroisePerlein <ambroise.perlein@hotmail.fr>
2020-03-05 09:54:13 +0100

    sql final 1.0

--- ProjWeb.sql
+++ ProjWeb.sql
@@ -31,22 +31,65 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL COMMENT 'clé primaire, identifiant numérique auto incrémenté',
-  `pseudo` varchar(20) CHARACTER SET latin1 NOT NULL COMMENT 'pseudo',
+  `identifiant` varchar(20) CHARACTER SET latin1 NOT NULL COMMENT 'identifiant',
   `passe` varchar(20) CHARACTER SET latin1 NOT NULL COMMENT 'mot de passe',
-  `admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'indique si l''utilisateur est un administrateur',
+  `admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'indique si l utilisateur est un administrateur ou si il est blacklist',
   `nom` varchar(20) CHARACTER SET latin1 NOT NULL COMMENT 'nom',
   `prenom` varchar(20) CHARACTER SET latin1 NOT NULL COMMENT 'prenom',
-  `mail` varchar(60) CHARACTER SET latin1 NOT NULL COMMENT 'mail',
-  `blacklist` tinyint(1) DEFAULT '0' COMMENT 'prenom'
+  `mail` varchar(60) CHARACTER SET latin1 NOT NULL COMMENT 'mail'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`id`, `pseudo`, `passe`, `admin`, `nom`, `prenom`, `mail`, `blacklist`) VALUES
-(1, 'Pierre', 'PDSM', 0, 'de Saint-Meleuc', 'Pierre', 'pinf.hyperion.hotmail.com', 0),
-(2, 'admin', 'admin', 1, 'admin', 'admin', 'pinf.hyperion.hotmail.com', 0);
+INSERT INTO `users` (`id`, `identifiant`, `passe`, `admin`, `nom`, `prenom`, `mail`) VALUES
+(1, 'Pierre', 'PDSM', 0, 'de Saint-Meleuc', 'Pierre', 'pinf.hyperion.hotmail.com'),
+(2, 'admin', 'admin', 1, 'admin', 'admin', 'pinf.hyperion.hotmail.com');
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livre_or`
+--
+
+CREATE TABLE `livre_or` (
+  `id` int(11) NOT NULL COMMENT 'clé primaire, identifiant numérique auto incrémenté',
+  `id_users` int(11) NOT NULL COMMENT 'l id du client du commentaire',
+  `date` date NOT NULL COMMENT 'date du commentaire',
+  `titre` varchar(50) CHARACTER SET latin1 NOT NULL COMMENT 'titre du commentaire fait par le client',
+  `texte` varchar(500) CHARACTER SET latin1 NOT NULL COMMENT 'texte du commentaire fait par le client'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `disponibilite`
+--
+
+CREATE TABLE `disponibilite` (
+  `id` int(11) NOT NULL COMMENT 'clé primaire, identifiant numérique auto incrémenté',
+  `date` date NOT NULL COMMENT 'jour de la disponibilite',
+  `heure_depart` time NOT NULL COMMENT 'heure du debut de la disponibilite',
+  `heure_fin` time NOT NULL COMMENT 'heure de fin de la disponibilite'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rendez_vous`
+--
+
+CREATE TABLE `rendez_vous` (
+  `id` int(11) NOT NULL COMMENT 'clé primaire, identifiant numérique auto incrémenté',
+  `id_users` int(11) NOT NULL COMMENT 'l id du client ayant pris un rendez_vous',
+  `id_rdv` int(11) NOT NULL COMMENT 'l id du rendez_vous',
+  `accepte` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'indique si l administrateur a accepte le rendez_vous (0 non, 1 oui)'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 
 --
 -- Index pour les tables exportées
@@ -59,6 +102,25 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `livre_or`
+--
+ALTER TABLE `livre_or`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `disponibilite`
+--
+ALTER TABLE `disponibilite`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `rendez_vous`
+--
+ALTER TABLE `rendez_vous`
+  ADD PRIMARY KEY (`id`);
+
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -67,6 +129,44 @@ ALTER TABLE `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'clé primaire, identifiant numérique auto incrémenté', AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `livre_or`
+--
+ALTER TABLE `livre_or`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'clé primaire, identifiant numérique auto incrémenté', AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `disponibilite`
+--
+ALTER TABLE `disponibilite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'clé primaire, identifiant numérique auto incrémenté', AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `rendez_vous`
+--
+ALTER TABLE `rendez_vous`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'clé primaire, identifiant numérique auto incrémenté', AUTO_INCREMENT=4;
+
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `articles`
+--
+ALTER TABLE `livre_or`
+  ADD CONSTRAINT `livre_or_users_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `articles`
+--
+ALTER TABLE `rendez_vous`
+  ADD CONSTRAINT `rendez_vous_users_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `rendez_vous_disponibilite_1` FOREIGN KEY (`id_rdv`) REFERENCES `disponibilite` (`id`) ON DELETE CASCADE;
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
